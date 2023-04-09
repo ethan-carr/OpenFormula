bd196b3b0dd780085ecd981a16030f8e46fc1205
ethan-carr <ethan@ethancarr.dev>
2023-04-08 23:30:05 -0400

    Adding some definitions and test cases.

---
+++ src/symbols.py
@@ -0,0 +1,296 @@
+arithmetic_trigonometric_latex_functions = [
+    "frac",
+    "dfrac"
+    "sqrt",
+    "cbrt",
+    "sin",
+    "cos",
+    "tan",
+    "csc",
+    "sec",
+    "cot",
+    "arcsin",
+    "arccos",
+    "arctan",
+    "log",
+    "ln",
+    "exp",
+    "gcd",
+    "lcm",
+    "arg",
+    "mod",
+    "bmod",
+    "pmod",
+    "times",
+    "div",
+    "pm",
+    "mp",
+    "cdot"
+]
+
+basic_arithmetic_symbols = [
+    "+",
+    "-",
+    "*",
+    "/",
+    "=",
+    "(",
+    ")",
+    "[",
+    "]",
+    "{",
+    "}",
+    "|"
+]
+
+
+latex_greek_symbols = [
+    "alpha",
+    "beta",
+    "gamma",
+    "delta",
+    "epsilon",
+    "varepsilon",
+    "zeta",
+    "eta",
+    "theta",
+    "vartheta",
+    "iota",
+    "kappa",
+    "lambda",
+    "mu",
+    "nu",
+    "xi",
+    "omicron",
+    "pi",
+    "varpi",
+    "rho",
+    "varrho",
+    "sigma",
+    "varsigma",
+    "tau",
+    "upsilon",
+    "phi",
+    "varphi",
+    "chi",
+    "psi",
+    "omega",
+
+    "Gamma",
+    "Delta",
+    "Theta",
+    "Lambda",
+    "Xi",
+    "Pi",
+    "Sigma",
+    "Upsilon",
+    "Phi",
+    "Psi",
+    "Omega"
+]
+
+latex_sub_superscripts = [
+    "_{}",
+    "^{}",
+    "'_{}'",
+    "'^{}'"
+]
+
+latex_brackets = [
+    "left(",
+    "right)",
+    "left[",
+    "right]",
+    "left\\{",
+    "right\\}",
+    "left|",
+    "right|"
+]
+
+latex_accents = [
+    "hat",
+    "check",
+    "tilde",
+    "acute",
+    "grave",
+    "dot",
+    "ddot",
+    "breve",
+    "bar",
+    "vec"
+]
+
+latex_fonts_styles = [
+    "mathrm",
+    "mathit",
+    "mathbf",
+    "mathsf",
+    "mathtt",
+    "mathbb",
+    "mathcal",
+    "mathfrak",
+    "boldsymbol",
+    "mathnormal",
+    "textrm",
+    "textit",
+    "textbf",
+    "textsf",
+    "texttt",
+    "emph",
+    "text"
+]
+
+latex_dots = [
+    "ldots",
+    "cdots",
+    "vdots",
+    "ddots"
+]
+
+latex_binom = [
+    "binom",
+    "dbinom",
+    "tbinom"
+]
+
+latex_misc_symbols = [
+    "aleph",
+    "hbar",
+    "imath",
+    "jmath",
+    "ell",
+    "wp",
+    "Re",
+    "Im",
+    "partial",
+    "infty",
+    "prime",
+    "emptyset",
+    "nabla",
+    "surd",
+    "top",
+    "bot",
+    "angle",
+    "triangle",
+    "forall",
+    "exists",
+    "nexists",
+    "neg",
+    "flat",
+    "natural",
+    "sharp",
+    "clubsuit",
+    "diamondsuit",
+    "heartsuit",
+    "spadesuit",
+    "mho",
+    "eth",
+    "hslash",
+    "circledS",
+    "Box",
+    "blacksquare",
+    "Diamond",
+    "lozenge",
+    "circ",
+    "bullet",
+    "oplus",
+    "ominus",
+    "otimes",
+    "oslash",
+    "odot",
+    "bigcirc",
+    "dagger",
+    "ddagger",
+    "amalg"
+]
+
+latex_matrix_environments = [
+    "array",
+    "matrix",
+    "pmatrix",
+    "bmatrix",
+    "Bmatrix",
+    "vmatrix",
+    "Vmatrix"
+]
+
+
+latex_operators = [
+    "oplus",
+    "ominus",
+    "otimes",
+    "oslash",
+    "odot",
+    "bigoplus",
+    "bigotimes",
+    "bigodot",
+    "nabla",
+    "partial"
+]
+
+latex_relations = [
+    "leq",
+    "geq",
+    "equiv",
+    "sim",
+    "simeq",
+    "approx",
+    "cong",
+    "neq",
+    "propto",
+    "parallel",
+    "perp",
+    "ll",
+    "gg",
+    "prec",
+    "succ",
+    "preceq",
+    "succeq",
+    "subset",
+    "supset",
+    "subseteq",
+    "supseteq",
+    "sqsubset",
+    "sqsupset",
+    "sqsubseteq",
+    "sqsupseteq",
+    "in",
+    "notin",
+    "ni",
+    "vdash",
+    "dashv",
+    "vDash",
+    "Vdash",
+    "Vvdash",
+    "models",
+    "mid",
+    "bowtie",
+    "Join"
+]
+
+latex_delimiters = [
+    "left.",
+    "right.",
+    "left<",
+    "right>",
+    "left\\lfloor",
+    "right\\rfloor",
+    "left\\lceil",
+    "right\\rceil",
+    "left\\langle",
+    "right\\rangle"
+]
+
+latex_spacing = [
+    "\\,",
+    "\\;",
+    "\\!",
+    "\\quad",
+    "\\qquad",
+    "\\hspace",
+    "\\vspace"
+]
+
+latex_symbols = [latex_misc_symbols, latex_greek_symbols]
+unsupported_functions = [latex_operators, latex_relations, latex_binom, latex_matrix_environments]
+blanked = [latex_accents, latex_spacing, latex_fonts_styles, latex_dots]
---
+++ src/testing.py
@@ -0,0 +1,12 @@
+test_latex_equations = [
+    "x + y = z",
+    "\\frac{a}{b} = \\frac{c}{d}",
+    "\\sqrt{x^2 + y^2} = r",
+    "\\sin(\\alpha + \\beta) = \\sin(\\alpha)\\cos(\\beta) + \\cos(\\alpha)\\sin(\\beta)",
+    "a^2 + b^2 = c^2",
+    "\\sum_{n=1}^{N} n = \\frac{N(N+1)}{2}",
+    "\\cos^2\\theta + \\sin^2\\theta = 1",
+    "\\tan(\\omega) = \\frac{\\sin(\\omega)}{\\cos(\\omega)}",
+    "\\log_b{a} \\times \\log_c{b} = \\log_c{a} \\times \\log_b{c}",
+    "\\frac{d}{dx}(x^n) = nx^{n-1}"
+]
